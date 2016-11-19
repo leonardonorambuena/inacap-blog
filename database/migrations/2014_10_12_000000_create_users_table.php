5ed724341c1091c3ca815df174428131cd71b707
lnorambuenadimabe <leonardo.norambuena@dimabe.cl>
2016-11-19 14:47:44 -0300

    Commit inicial de la primera clase con creación de modelos y migraciones

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_name')->unique();
+            $table->string('first_name',50);
+            $table->string('last_name', 50);
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('section');
+            $table->string('phone');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Laravel PHP Framework
+
+Proyecto basico desarrollado en Laravel versión 5.3 con fines educativos impartido por Leonardo Norambuena en Inacap Sede Rancagua, 
+
+## para copiar este proyecto los alumnos deben ejecutar los siguientes comandos:
+
+1- php artisan key:generate
+2- composer update
+
+cualquier problema contactarse con Leonardo Norambuena al correo leonardo.norambuena02@inacapmail.cl
+
